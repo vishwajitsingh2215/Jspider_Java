158b32311b743dc4e939a1e2db63467dd67d5c4e
Vishwajit <vishwajitsingh2215@gmail.com>
2024-04-04 21:55:41 +0530

    Added one more class with Static method and variables

---
+++ Non Static Program/src/com/Person2.java
@@ -0,0 +1,10 @@
+package com;
+
+class Person2 {
+	String name="Nami";
+	String gender="Female";
+	void park() {
+		System.out.println(name+" who is walking in the park is a "+gender);
+	}
+		
+}
---
+++ Static Program/src/com/School.java
@@ -0,0 +1,12 @@
+package com;
+
+class School {
+	static String name="Zoro";
+	static int roll=3;
+	static String school_name="One-piece";
+	static void study() {
+		System.out.println("Roll no. :"+roll);
+		System.out.println("Name is "+name);
+		System.out.println("Study in the School "+school_name);
+	}
+}
