e6a2908a0eefe5bfe93ec53922a4a6894c865c11
Vishwajit <vishwajitsingh2215@gmail.com>
2024-04-08 23:25:21 +0530

    Taking array input and displaying

--- Array Programs/src/com/Test.java
+++ Array Programs/src/com/Test.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.Scanner;
+
 class Test {
 	public static void main(String[] args) {
 		int[] a= {10,20,30};
@@ -11,5 +13,18 @@ class Test {
 		for(int i=0;i<subjects.length;i++) {
 			System.out.println(i+"-->"+subjects[i]);
 		}
+		System.out.println("_____________________");
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the size for array");
+		int size= sc.nextInt();
+		String[] sub = new String[size];
+		System.out.println("Enter  "+size+" subjects name");
+		for(int i=0;i<size;i++) {
+			sub[i]= sc.next();
+		}
+		System.out.println("Here is the Subjects");
+		for(int i=0;i<sub.length;i++) {
+			System.out.println("Subject : "+sub[i]+" ");
+		}sc.close();
 	}
 }
